491e1a2683c75e410bf7da08b2435e2a389f4e9c
Prakhar Newatia <40995798+PrakNew@users.noreply.github.com>
2022-09-20 09:58:41 +0530

    Create Maximum Length of Repeated Subarray.py

---
+++ Maximum Length of Repeated Subarray.py
@@ -0,0 +1,14 @@
+#https://leetcode.com/problems/maximum-length-of-repeated-subarray/
+
+class Solution:
+    def findLength(self, nums1: List[int], nums2: List[int]) -> int:
+        n,m=len(nums1),len(nums2)
+        dp=[[0]*(m+1) for _ in range(n+1) ]
+        ma=0
+        for x in range(n):
+            for y in range(m):
+                if nums1[x]==nums2[y]:
+                    dp[x][y]=dp[x-1][y-1]+1
+                    ma=max(ma,dp[x][y])
+        return ma
+ 
