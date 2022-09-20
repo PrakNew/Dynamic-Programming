94750d972bcaebccdcd2edc4965646ba3a4b92cb
Prakhar Newatia <40995798+PrakNew@users.noreply.github.com>
2022-09-20 10:02:59 +0530

    Create lcs.py

---
+++ lcs.py
@@ -0,0 +1,44 @@
+#import sys
+#sys.setrecursionlimit(1000000)
+def lcs_length(x,y,m,n):
+	c=[list([0]*(n+1))[:] for x in range(m+2)]
+	#b=[list([0]*(n+1))[:] for x in range(m+2)]
+	for i in range(1,m+1):
+		for j in range(1,n+1):
+			if x[i]==y[j]:
+				c[i][j]=c[i-1][j-1]+1
+				#b[i][j]="upleft"
+			elif c[i-1][j]>=c[i][j-1]:
+				c[i][j]=c[i-1][j]
+				#b[i][j]="up"
+			else:
+				c[i][j]=c[i][j-1]
+				#b[i][j]="left"
+	return c
+q3=input()
+q4=input()
+q1="0"+q3
+q2="0"+q4
+a=lcs_length(q1,q2,len(q1)-1,len(q2)-1)
+#p=""
+print(a[len(q1)-1][len(q2)-1])
+'''print(b)
+c=0
+def print_lcs(b,x,i,j):
+	#global p
+	global c
+	#p=""
+	if i==0 or j==0:
+		return
+	if b[i][j]=="upleft":
+		print_lcs(b,x,i-1,j-1)
+		#print(x[i-1])
+		c=c+1
+		#p=p+x[i-1]
+	elif b[i][j]=="up":
+		print_lcs(b,x,i-1,j)
+	else:
+		print_lcs(b,x,i,j-1)
+print_lcs(b,q3,len(q1)-1,len(q2)-1)
+#print(p)
+print(c)'''
